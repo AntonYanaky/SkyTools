bdf78f92196468fd135d3c223ccdf0f7f7bf52d1
Anton Yanaky <171488152+AntonYanaky@users.noreply.github.com>
2025-08-03 15:24:14 -0400

    Ready for GHpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from './pages/Home.tsx';
 import BitsMain from './pages/BitsMain.tsx';
 
 export default function App() {
 
   return (
-    <Routes>
+    <BrowserRouter basename="/SkyTools/" >
       <Route path="/" element={<Home />} />
       <Route path="/bits" element={<BitsMain />} />
-    </Routes>
+    </BrowserRouter>
   )
 }
