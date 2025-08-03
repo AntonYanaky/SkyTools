97c342a73c564849617910f574a5eb80ce1ab261
Anton Yanaky <171488152+AntonYanaky@users.noreply.github.com>
2025-08-03 15:42:00 -0400

    Stupid mistake

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home.tsx';
 import BitsMain from './pages/BitsMain.tsx';
 
 export default function App() {
 
   return (
-    <BrowserRouter basename="/SkyTools" >
+    <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/bits" element={<BitsMain />} />
-    </BrowserRouter>
+    </Routes>
   )
 }
